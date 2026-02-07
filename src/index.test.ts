b9fe3f6dd6b226a8cbc074644077daaf9eb738c8
Jaroslaw Weber <jaroslaw.weber@gmail.com>
2026-02-07 11:41:19 +0900

    Add tests, README, and MIT license

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 import { describe, it, expect } from 'bun:test'
-import app from './index'
+import { app } from './index'
 
 describe('Hello World', () => {
   it('responds with Hello World on GET /', async () => {
